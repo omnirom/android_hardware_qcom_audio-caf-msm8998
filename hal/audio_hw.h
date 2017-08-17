814594fca18685d0ed4d3f839eb99defeaa054fe
Humberto Borba <humberos@gmail.com>
2017-08-17 12:30:14 -0300

    Revert "audio: Add check to confirm if routing to BT SCO is successfull"
    This reverts commit 22f1727ae11a0919cc165c01df93c4c5f93d1de3.
    
    Change-Id: Ic979e4327c89eb4e13ed6b9fe4c419c9ba838369

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -258,7 +258,6 @@ struct stream_out {
     struct listnode qaf_offload_cmd_list;
     uint32_t platform_latency;
     audio_offload_info_t info;
-    int started;
 };
 
 struct stream_in {
@@ -430,7 +429,6 @@ struct audio_device {
     bool asrc_mode_enabled;
     qahwi_device_t qahwi_dev;
     bool vr_audio_mode_enabled;
-    bool bt_sco_on;
 };
 
 int select_devices(struct audio_device *adev,
